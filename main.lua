ca8303006bf244f98b54e96247660b5707a1ae71
flaribbit <1149761294@qq.com>
2020-01-09 20:36:43 +0800

    重力

--- main.lua
+++ main.lua
@@ -10,47 +10,7 @@ end
 
 function love.update(dt)
     Control:Update()
-    if Control:IsPress("left") then
-        if Field:Check(Block.data,Block.i,Block.j-1) then
-            Block.j=Block.j-1
-        end
-    end
-    if Control:IsPress("right") then
-        if Field:Check(Block.data,Block.i,Block.j+1) then
-            Block.j=Block.j+1
-        end
-    end
-    if Control:IsPress("up") then
-        while Field:Check(Block.data,Block.i+1,Block.j) do
-            Block.i=Block.i+1
-        end
-        Field:Lock(Block)
-    end
-    if Control:IsPress("down") then
-        if Field:Check(Block.data,Block.i+1,Block.j) then
-            Block.i=Block.i+1
-        end
-    end
-    if Control:IsPress("rotl") then
-        Block:RotateL()
-    end
-    if Control:IsPress("rotr") then
-        Block:RotateR()
-    end
-    if Control:IsPress("hold") and Block.canhold then
-        local hold=Next[0]
-        Block.canhold=false
-        Next[0]=Block.type
-        if hold>0 then
-            Block:Load(hold)
-        else
-            Block:Load(Next:Shift())
-        end
-    end
-    if Control:IsPress("load") then
-        print("["..os.date("%Y-%m-%d %H:%M:%S").."] Loaded")
-        dofile("test.lua")
-    end
+    GameUpdate()
 end
 
 function love.draw(dt)
